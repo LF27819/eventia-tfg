75aa7a880647d561cbc436159fb9dc723b4ea73d
LF27819 <luciafeliperuiz@gmail.com>
2026-06-01 14:37:27 +0200

    Detalle de evento

--- eventia-frontend/src/routes/AppRouter.tsx
+++ eventia-frontend/src/routes/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import EventsPage from "../pages/EventsPage";
+import EventDetailPage from "../pages/EventDetailPage";
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/eventos" element={<EventsPage />} />
+      <Route path="/eventos/:id" element={<EventDetailPage/>} />
     </Routes>
   );
 }
